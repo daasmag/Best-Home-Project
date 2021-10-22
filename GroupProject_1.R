f7f8e57be0125fc0e930a967e630cb31e28d0d56
daasmag <93010959+daasmag@users.noreply.github.com>
2021-10-22 16:14:58 -0400

    Update GroupProject_1.R

--- GroupProject_1.R
+++ GroupProject_1.R
@@ -2,4 +2,5 @@ bestHome <- read.csv(file = "BestHomePart1.csv",
                      header = T, as.is = T)
 
 margin <- c(bestHome$Selling_price-bestHome$Purchasing_cost)
-margin
+bestHome$gross_margin <- margin
+bestHome
